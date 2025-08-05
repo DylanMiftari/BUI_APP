0b1400292e510d787ac25448a02c90d7396fb388
DylanMiftari <mifdylan512@gmail.com>
2025-08-05 18:32:32 +0200

    feat: Create background and header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BUI_APP';
+  defaultBackground: string = "--green-gradiant";
+  routeBackground: Record<string, string> = {
+  };
+
+  background: string = "";
+
+  constructor(private router: Router) {
+    
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const route = this.router.url.split('?')[0];
+        const splittedRoute = route.split("/");
+        this.background = this.routeBackground[splittedRoute[1]] || this.defaultBackground;
+      }
+    });
+  }
 }
